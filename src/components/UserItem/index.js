717a64f5f47560e3af482897b49fc03313bdefc1
Dhrumil2910 <dhrumil2910@gmail.com>
2017-04-02 12:31:03 +0530

    Fixes in items and trade

--- src/components/UserItem/index.js
+++ src/components/UserItem/index.js
@@ -18,10 +18,10 @@ class UserItem extends Component {
             </h3>
             <p className="itemCost frm">$40</p>
             <p className="addDate frm">23 Jan, 2017</p>
-            <p className="itemDescription">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque nihil dicta temporibus incidunt fugit culpa similique ipsum sit? Hic ad beatae quidem repudiandae dignissimos tenetur consequuntur, ullam, accusantium earum at.</p>
+            <p className="itemDescription">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque nihil dicta temporibus incidunt fugit culpa similique ipsum sit? Hic ad beatae quidem repudiandae dignissimos tenetur consequuntur, ullam, accusantium earum at.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque nihil dicta temporibus incidunt fugit culpa similique ipsum sit? Hic ad beatae quidem repudiandae dignissimos tenetur consequuntur, ullam, accusantium earum at.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque nihil dicta temporibus incidunt fugit culpa similique ipsum sit? Hic ad beatae quidem repudiandae dignissimos tenetur consequuntur, ullam, accusantium earum at.</p>
             <div className="tradeBtnWrapper lower">
-              <button className="deleteBtn normalBtn">Remove Item</button>
-              <button className="editBtn normalBtn" onClick={this.props.editModal}>Edit Info</button>
+              <button className="deleteBtn normalBtn">Delete Item</button>
+              <button className="editBtn normalBtn" onClick={this.props.editModal}>Edit</button>
             </div>
           </div>
         </div>
